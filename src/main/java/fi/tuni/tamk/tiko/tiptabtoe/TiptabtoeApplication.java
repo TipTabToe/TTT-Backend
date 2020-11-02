71bd51fc41e3852f37dc1e0b245309908fbd3652
Heikki Kangas <heikki. kangas@tuni.fi>
2020-11-02 22:16:10 +0200

    Initial commit

---
+++ src/main/java/fi/tuni/tamk/tiko/tiptabtoe/TiptabtoeApplication.java
@@ -0,0 +1,13 @@
+package fi.tuni.tamk.tiko.tiptabtoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiptabtoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiptabtoeApplication.class, args);
+	}
+
+}
